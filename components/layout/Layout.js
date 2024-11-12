10d61f090ad16ab15043615df569a75a3204cd45
HoTaN <id4acc2014@protonmail.com>
2024-11-12 22:36:09 +0330

    feat: :sparkles: I created and integrated layout component
    I created the Layout component with header and footer sections, linking to "Menu" and "Categories" pages.
    Added CSS module styling in Layout.module.css for layout styling.
    Integrated Layout into _app.js, wrapping all pages with a consistent layout structure.

---
+++ components/layout/Layout.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.left}>
+          <Link href="/">Foodista</Link>
+        </div>
+        <div className={styles.right}>
+          <Link href="/menu">Menu</Link>
+          <Link href="/categories">Categories</Link>
+        </div>
+      </header>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}>Made with ❤️ by Foodista &copy; 2024</footer>
+    </>
+  );
+}
+
+export default Layout;
